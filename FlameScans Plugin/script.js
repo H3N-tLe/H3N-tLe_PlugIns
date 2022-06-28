bdd3503d7505843f13d4457b09fb0191f8ff141b
RC-14 <61058098+RC-14@users.noreply.github.com>
2022-06-28 10:54:01 +0200

    Add loadImage function and use allowRemoteContent

--- FlameScans Plugin/script.js
+++ FlameScans Plugin/script.js
@@ -17,6 +17,41 @@ const cloudflare = {
 	}
 }
 
+const loadImage = (url) => {
+	// Check if the parameter is valid
+	if (typeof url !== 'string' && !(url instanceof URL)) {
+		throw new Error('url is not a string or URL');
+	} else if (typeof url === 'string') {
+		try {
+			url = new URL(url);
+		} catch (error) {
+			throw new Error('url is not a valid URL');
+		}
+	}
+
+	const image = { ext: url.split('.').pop(), b64: '' };
+
+	return new Promise((resolve, reject) => {
+		fetch(url).then((response) => {
+			// Check if the response is valid
+			if (response.headers.get("content-type").startsWith('text/html')) {
+				disallowRemoteContent();
+				cloudflare.bypass();
+				reject(new Error('Got HTML response'));
+			}
+
+			return response.blob();
+		}).then((blob) => {
+			const reader = new FileReader();
+			reader.onload = () => {
+				image.b64 = reader.result.split(';base64,').pop();
+				resolve(image);
+			};
+			reader.readAsDataURL(blob);
+		});
+	});
+}
+
 const loadImages = (urls) => {
 	// Check if the parameter is valid
 	if (!Array.isArray(urls)) {
@@ -35,29 +70,14 @@ const loadImages = (urls) => {
 			resolve(images);
 		};
 
-		for (let i = 0; i < urls.length; i++) {
-			images.push({ ext: urls[i].split('.').pop(), b64: '' });
-
-			fetch(urls[i]).then((response) => {
-				// Check if the response is valid
-				if (!response.ok) {
-					return;
-				} else if (response.headers.get("content-type").startsWith('text/html')) {
-					disallowContent();
-					location.reload();
-				}
-
-				return response.blob();
-			}).then((blob) => {
-				const reader = new FileReader();
-				reader.onload = () => {
-					images[i].b64 = reader.result.split(';base64,').pop();
-					done();
-				};
-				reader.readAsDataURL(blob);
+		urls.forEach((url) => {
+			loadImage(url).then((image) => {
+				images.push(image);
+				done();
+			}).catch((error) => {
+				reject(error);
 			});
-
-		}
+		});
 	});
 };
 
@@ -94,7 +114,7 @@ isJSAllowed().then((jsAllowed) => {
 		info.urls = data.sources['0'].images;
 	});
 
-	allowContent();
+	allowRemoteContent();
 	isContentAllowed().then((contentAllowed) => {
 		if (!contentAllowed) {
 			location.reload();
